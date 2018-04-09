322fbc4564a7addae33fd9cffd682985b6140807
Janek <jan.wielgus12@op.pl>
2018-04-09 18:37:47 +0200

    Wyświetlanie temperatury i wilgotności na wyświetlaczu + gitignore

--- IntelligentGardenWateringSystem/IntelligentGardenWateringSystem.ino
+++ IntelligentGardenWateringSystem/IntelligentGardenWateringSystem.ino
@@ -1,13 +1,63 @@
+/*
+	Date: 09.04.2018
+	Author: Jan Wielgus
+	Intelligent garden watering system
+*/
+
+#include <DHT.h>
+#include <Wire.h>
+#include <LiquidCrystal_I2C/LiquidCrystal_I2C.h>
+#include "config.h"
+
+LiquidCrystal_I2C lcd(LCD_ADDRESS, 16, 2);
+DHT dht(DHTPIN, DHTTYPE);
+
+void readDht();
+void printText(const char* text);
+
+float temperature;
+float humidity;
+
+
 void setup()
 {
-
-  /* add setup code here */
-
+	lcd.init();
+	dht.begin();
+	
+	
+	lcd.backlight();
+	printText(String("Hello"));
+	delay(500);
 }
 
 void loop()
 {
-
-  /* add main program code here */
-
+	readDht();
+	//lcd.clear();
+	lcd.setCursor(0,0);
+	printText(tempText);
+	printText(String(temperature));
+	lcd.setCursor(0,1);
+	printText(humidText);
+	printText(String(humidity));
+	delay(1000);
 }
+
+
+void readDht()
+{
+	temperature = dht.readTemperature();
+	humidity = dht.readHumidity();
+	/* Sprawdzanie poprawnoœci: isnan(t) albo isnan(h): true-blad */
+}
+
+void printText(String text)
+{
+	for (int i=0; i < text.length(); i++)
+		lcd.print(text[i]);
+}
+
+
+/*
+	lcd.setCursor(col, row) 
+*/
---
+++ IntelligentGardenWateringSystem/config.h
@@ -0,0 +1,23 @@
+/*
+ * config.h
+ *
+ * Created: 09/04/2018 16:59:35
+ *  Author: Jan Wielgus
+ */ 
+
+
+#ifndef CONFIG_H_
+#define CONFIG_H_
+
+#define DHTPIN 4
+#define DHTTYPE DHT11
+#define LCD_ADDRESS 0x3F
+
+
+String tempText = "Temp: ";
+String humidText = "Humid: ";
+
+
+
+#endif /* CONFIG_H_ */
+
