f743bd6265db673dbab2160b563105b41a093d2f
Venterek <jan.wielgus12@op.pl>
2018-06-27 18:27:14 +0200

    Napisana funkcja do odbierania sygnału z pilota (#1)
    Odbieranie poprzez użycie funkcji receiveData() w loop.
    
    Jednorazowe pobranie odebranego przycisku wywołując funkcję getPressedBtn().

--- IntelligentGardenWateringSystem/config.h
+++ IntelligentGardenWateringSystem/config.h
@@ -13,6 +13,14 @@
 #define DHTTYPE DHT11
 #define LCD_ADDRESS 0x3F
 
+// Buttons received codes
+#define BTN1 10420129 // A
+#define BTN2 10420130 // B
+#define BTN3 10420132 // C
+#define BTN4 10420136 // D
+
+#define MIN_TIME_BTWN_BTN 350 // Minimalny czas pomiedzy kliknieciami (zeby uniknac powturzonego odebrania)
+
 
 String tempText = "Temp: ";
 String humidText = "Humid: ";
